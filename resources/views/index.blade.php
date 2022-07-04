0a556d12f67df6954bffad4295dc177805b2d0b4
ramezani84 <a.ramezani84@gmail.com>
2022-07-04 23:49:51 +0430

    home page snappfood

---
+++ resources/views/index.blade.php
@@ -0,0 +1,133 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="SnapFood WebSite">
+    <meta name="keywords" content="HTML, CSS, JavaScript SnapFood">
+    <meta name="author" content="Aghdasi & Ramezani">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="row1">
+    <div class="col1-left">
+        <img src="images/pizza.png" alt="pizza">
+    </div>
+    <div class="col1-right">
+        <img src="images/snappfood.png" alt="">
+    </div>
+    <div class="row-small">
+        <div class=col-small>
+            <img src="images/images-small/desktop_1_603508bf202d8_img_st_food.png" alt="">
+            <p> ساندویچ</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_2_603508b330711_img_st_cafe.png" alt="">
+            <p> کافه</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_3_603508a95b9be_img_st_sweet.png" alt="">
+            <p> شیرینی</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_4_603508a14ab73_img_st_supermarket.png" alt="">
+            <p> سوپرمارکت</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_5_60350898c61b5_img_st_bakery.png" alt="">
+            <p> نانوایی</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_6_6035088cbcde4_img_st_fruit.png" alt="">
+            <p> میوه</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_7_60350883d6e43_img_st_nut.png" alt="">
+            <p> آجیل</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_8_6035087b463a3_img_st_icecream.png" alt="">
+            <p> آبمیوه بستنی</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_9_603b811b1d540_img_st_other2.png" alt="">
+            <p> سایر</p>
+        </div>
+        <div class=col-small>
+            <img src="images/images-small/desktop_11_603507afc9a32_img_st_meat.png" alt="">
+            <p> پروتئین</p>
+        </div>
+    </div>
+</div>
+<div class="row2">
+    <div class="col2">
+        <img src="images/website_image_asian_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_burger_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_fastfood_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_gilani_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_irani_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_italy_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_kebab_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_pizza_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_salad_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_sandwich_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_seafood_1.jpg" alt="">
+    </div>
+    <div class="col2">
+        <img src="images/website_image_sokhari_1.jpg" alt="">
+    </div>
+</div>
+<div class="row3">
+    <div class="col3-left">
+        <img src="images/mobile.png" alt="">
+    </div>
+    <div class="col3-right">
+        <div class="row3-top">
+            <h1>اپلیکیشن اسنپ‌فود</h1>
+            <h3>با اپلیکیشن اسنپ‌فود به راحتی و با چند کلیک ساده می‌توانید رستوران‌ها، کافه‌ها، شیرینی‌فروشی‌ها و
+                سوپرمارکت‌های نزدیک خودتان را جست‌و‌جو کرده و از تجربه سفارش آسان از اسنپ‌فود لذت ببرید.</h3>
+        </div>
+        <div class="row3-bottom">
+            <a href="#">
+                <img src="images/tamas%20ba%20ma.jpg" alt="">
+            </a>
+        </div>
+    </div>
+</div>
+<div class="row4">
+
+    <div class="col4-left">
+        <img src="images/vendor_pic.png" alt="">
+    </div>
+    <div class="col4-right">
+        <div class="row4-top">
+            <h1>صاحب کسب و کار هستید؟</h1>
+            <h3>با اسنپ فود کسب و کارتان را آنلاین کنید و فروشتان را افزایش دهید.</h3>
+        </div>
+        <div class="row4-bottom">
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+//Route::get('/', [HomeController::class,'index']);
+Route::get('/', 'HomeController@index');
+
